a17d6fc4d06e92c2c68747026f82ebe678128b80
Daniel Bälz <me@dbaelz.de>
2021-08-20 15:52:58 +0200

    Initial commit with auto-generated content and minor improvements

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
+}
+
+group = "de.dbaelz.stellar"
+version = "1.0"
+
+repositories {
+    google()
+    jcenter()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "de.dbaelz.stellar.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Stellar Presentation"
+            packageVersion = "1.0.0"
+        }
+    }
+}
