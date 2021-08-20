43f4cc8bd57f2938a8df069a9d555a21926393a9
Daniel Bälz <me@dbaelz.de>
2021-08-20 16:02:17 +0200

    Use new Window API

--- src/main/kotlin/de/dbaelz/stellar/Main.kt
+++ src/main/kotlin/de/dbaelz/stellar/Main.kt
@@ -1,22 +1,33 @@
 package de.dbaelz.stellar
 
-import androidx.compose.desktop.Window
-import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
 
-fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(
+            placement = WindowPlacement.Floating,
+            size = WindowSize(1024.dp, 768.dp)
+        ),
+        title = "Stellar Presentation",
+        resizable = true
+    ) {
+        var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        MaterialTheme {
+            Button(onClick = {
+                text = "Hello, Desktop!"
+            }) {
+                Text(text)
+            }
         }
     }
 }
