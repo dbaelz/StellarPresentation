1bee8fe3ff1850203632b4a20815245b6c3bc489
Daniel Bälz <me@dbaelz.de>
2021-08-20 16:30:14 +0200

    Switch to fullscreen and back when F11 is pressed

--- src/main/kotlin/de/dbaelz/stellar/Main.kt
+++ src/main/kotlin/de/dbaelz/stellar/Main.kt
@@ -1,18 +1,44 @@
 package de.dbaelz.stellar
 
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 import de.dbaelz.stellar.theme.StellarPresentationTheme
 
+@ExperimentalComposeUiApi
 fun main() = application {
+    val windowState = rememberWindowState(
+        placement = WindowPlacement.Floating,
+        size = WindowSize(1024.dp, 768.dp)
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
-        state = rememberWindowState(
-            placement = WindowPlacement.Floating,
-            size = WindowSize(1024.dp, 768.dp)
-        ),
+        state = windowState,
         title = "Stellar Presentation",
-        resizable = true
+        resizable = true,
+        onKeyEvent = {
+            when {
+                it.key == Key.F11 -> {
+                    // Ignore key up (release) and only react when the key is pressed
+                    if (it.type == KeyEventType.KeyUp) return@Window false
+
+                    // Known issue: Fullscreen always switches to Floating even
+                    // if previously it was Maximized
+                    if (windowState.placement == WindowPlacement.Fullscreen) {
+                        windowState.placement = WindowPlacement.Floating
+                    } else {
+                        windowState.placement = WindowPlacement.Fullscreen
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
     ) {
         StellarPresentationTheme {
 
