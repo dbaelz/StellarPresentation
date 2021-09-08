8b7d9abf95ef7fea5f3f45fd1a2dd65e81d639ea
Daniel Bälz <me@dbaelz.de>
2021-09-08 18:16:01 +0200

    ImageSlide: Use modifier for image and surround it with a box to be more flexible

--- src/main/kotlin/de/dbaelz/stellar/feature/presentation/ImageSlide.kt
+++ src/main/kotlin/de/dbaelz/stellar/feature/presentation/ImageSlide.kt
@@ -1,15 +1,19 @@
 package de.dbaelz.stellar.feature.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 
 @Composable
 fun ImageSlide(
     title: String,
     image: Painter,
+    contentScale: ContentScale = ContentScale.Fit,
     modifier: Modifier = Modifier,
     footer: @Composable () -> Unit = {}
 ) {
@@ -18,10 +22,17 @@ fun ImageSlide(
         modifier = Modifier,
         footer = footer
     ) {
-        Image(
-            modifier = modifier.fillMaxSize(),
-            painter = image,
-            contentDescription = null,
-        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                modifier = modifier,
+                painter = image,
+                contentDescription = null,
+                contentScale = contentScale
+            )
+        }
+
     }
 }
